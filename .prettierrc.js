fe0f63907afbe39a7c53215c1c3d779eb4a4b6b0
Ryan Beerwinkle <70540400+rbeer172@users.noreply.github.com>
2021-06-07 16:23:02 -0500

    Add files via upload

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    semi: true,
+    trailingComma: 'es5',
+    singleQuote: true,
+    tabWidth: 4,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    endOfLine:"auto"
+  }
